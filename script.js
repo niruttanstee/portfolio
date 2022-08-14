e51229d3040cd4d375e188e873fc3b62bcb1bf32
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-08-14 23:59:13 +0100

    Add preliminary preloader

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* preloader */
 let animation = bodymovin.loadAnimation({
     container: document.getElementById("preloader"),
-    renderer: 'svg',
+    renderer: 'canvas',
     loop: false,
     autoplay: true,
     path: 'https://assets2.lottiefiles.com/packages/lf20_n8mnycdf.json'
