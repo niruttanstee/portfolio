f50456ade75129ae1ab2aa0b5397c42a38d34553
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-08-14 23:06:24 +0100

    Preloader test

---
+++ script.js
@@ -0,0 +1,8 @@
+/* preloader */
+let animation = bodymovin.loadAnimation({
+    container: document.getElementById("preloader"),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/packages/lf20_n8mnycdf.json'
+})
