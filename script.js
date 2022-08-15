02b912742a2b72d403a2ca2e8d18328bc5726a3d
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-08-15 16:42:48 +0100

    Update preloader
    Preloader animation has now been updated with two styles for light mode and dark mode dynamically switching between the two depending on the time.
    
    9am - 17pm (light mode)
    18pm - 8am (dark mode)

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 /* preloader */
 function preloader() {
+    let preloaderContainer = document.getElementById("preloader");
+    const currentDate = new Date();
+    let currentHour = currentDate.getHours();
+    if (currentHour > 8 && currentHour < 18) {
+        preloaderContainer.style.backgroundColor = "#F0F1F2";
+        lightPreloader();
+    } else {
+        preloaderContainer.style.backgroundColor = "#1E1E1E";
+        darkPreloader();
+    }
+}
+function lightPreloader() {
     let animation = bodymovin.loadAnimation({
         container: document.getElementById("preloader"),
-        renderer: 'canvas',
+        renderer: 'svg',
         loop: false,
         autoplay: true,
-        path: 'https://assets5.lottiefiles.com/packages/lf20_hzxei5jl.json'
+        path: 'https://assets6.lottiefiles.com/packages/lf20_nziwu7b6.json'
     })
-    setTimeout(hidePreloader, 2500);
-
 }
-function hidePreloader () {
-    let preloader = document.getElementById("preloader");
-    preloader.style.display = "none";
+function darkPreloader(preloaderContainer) {
+    let animation = bodymovin.loadAnimation({
+        container: document.getElementById("preloader"),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'https://assets4.lottiefiles.com/packages/lf20_grojqc5l.json'
+    })
 }
