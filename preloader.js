cdc7eb3f878f46edffc05e3b43a3d4bdc1d8e87c
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-08-15 17:06:48 +0100

    Fixed preloader bug

--- preloader.js
+++ preloader.js
@@ -1,24 +1,7 @@
 /* preloader */
 function preloader() {
     let preloaderContainer = document.getElementById("preloader");
-    const currentDate = new Date();
-    let currentHour = currentDate.getHours();
-    if (currentHour > 8 && currentHour < 18) {
-        preloaderContainer.style.backgroundColor = "#F0F1F2";
-        lightPreloader();
-    } else {
-        preloaderContainer.style.backgroundColor = "#1E1E1E";
-        darkPreloader();
-    }
-}
-function lightPreloader() {
-    let animation = bodymovin.loadAnimation({
-        container: document.getElementById("preloader"),
-        renderer: 'svg',
-        loop: false,
-        autoplay: true,
-        path: 'https://assets6.lottiefiles.com/packages/lf20_nziwu7b6.json'
-    })
+    darkPreloader();
 }
 function darkPreloader(preloaderContainer) {
     let animation = bodymovin.loadAnimation({
